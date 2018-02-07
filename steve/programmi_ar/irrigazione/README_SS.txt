72e7c133a2cf87f476ca6eacb10b7cb13e0f747f
stevetdr <stevetdr@gmail.com>
2018-02-07 09:48:31 +0100

    Salvato il 07-02-2018 alle 09:48:29

---
+++ steve/programmi_ar/irrigazione/README_SS.txt
@@ -0,0 +1,3 @@
+attenzione, sistemato platformio.ini
+e' da verificare come mettere il linea solo una versione di questo programma
+altrimenti devo copiarlo su tutte li directory dei vari programmi
--- steve/programmi_ar/pointer/src/pointer.ino
+++ steve/programmi_ar/pointer/src/pointer.ino
@@ -1,66 +1,53 @@
  #include <stdio.h>
 
-int counter = 0;
 
 void setup() {
+    //int a = 10;
+    int *myPointer;     // definisce il pointer
+    //char myVar[]={'A','B','C','D','E','F','G','H','I','J','\0'};    // crea array
+    int myVar[]={1,2,3,4,9,3,7,2,5,1,3};
+
     Serial.begin(9600);
-    // -------------------------------------------------------------------------
-    int test1 = 10;             // definisco la variabile
-    int   *ptr1;                // definisco il pointer
-    ptr1 = &test1;              // ptr1 contiene l'indirizzo fisico di test1
-    // -------------------------------------------------------------------------
-    char test2 = 90;             // definisco la variabile
-    char   *ptr2;                // definisco il pointer
-    ptr2 = &test2;              // ptr1 contiene l'indirizzo fisico di test1
-    // -------------------------------------------------------------------------
-    float test3 = 3.141517121;             // definisco la variabile
-    float   *ptr3;                // definisco il pointer
-    ptr3 = &test3;              // ptr1 contiene l'indirizzo fisico di test1
-    // -------------------------------------------------------------------------
-    int array[100];             // definisco matrice
-    array[1] = {9};
-    int *ptr_arr;                // definisco il pointer
-    ptr_arr = array;            // indirizzo in memoria, corrisponde a ptr_arr = &array[0]
 
-    // -------------------------------------------------------------------------
-    Serial.println("--");
-    Serial.print(" Contenuto della variabile test1 / *ptr1 = "); Serial.print(test1);Serial.print(" / "); Serial.println(*ptr1);      //
-    Serial.print(" Ptr1 / &test1  in memoria in Hex: "); Serial.print( (long) ptr1, HEX);Serial.print(" / "); Serial.println( (long) &test1, HEX);
-    Serial.println("--");   // indirizzo fisico memoria
-    // -------------------------------------------------------------------------
-    Serial.print(" Contenuto della variabile test2 / *ptr2 = "); Serial.print(test2);Serial.print(" / "); Serial.println(*ptr2);      //
-    Serial.print(" Ptr2 / &test2  in memoria in Hex: "); Serial.print( (long) ptr2, HEX);Serial.print(" / "); Serial.println( (long) &test2, HEX);
-    Serial.println("--");
-    // -------------------------------------------------------------------------
-    Serial.print(" Contenuto della variabile test3 / *ptr3 = "); Serial.print(test3);Serial.print(" / "); Serial.println(*ptr3);      //
-    Serial.print(" Pointer in memoria in Hex: "); Serial.print( (long) ptr3, HEX);Serial.print(" / ");Serial.println( (long) &test3, HEX);      // indirizzo fisico memoria
-    Serial.println("--");
-    // -------------------------------------------------------------------------
-    Serial.print(" Contenuto della variabile array= "); Serial.println(array[1]);       //
-    Serial.print(" =  contenuto del pointer: "); //Serial.println(*array[1]);
-    Serial.print(" Pointer in memoria in Hex: "); Serial.println( (long) ptr_arr, HEX);    // indirizzo fisico memoria
-    Serial.print(" =  pointer in memoria in Hex: "); Serial.println( (long) &ptr_arr[0], HEX);   // "
-    Serial.println("");
-    // -------------------------------------------------------------------------
-    Serial.println("--***--");
-    Serial.print(" sizeof di test1 : ");Serial.println(sizeof(test1));
-    Serial.print(" sizeof di test2 : ");Serial.println(sizeof(test2));
-    Serial.print(" sizeof di test3 : ");Serial.println(sizeof(test3));
-    Serial.print(" sizeof di array : ");Serial.println(sizeof(array));
-    //float test2 = 3.14159;
-    //char test3[ ] = "This is a character array";
-    //float *ptr2;
-    //char  *ptr3;
+    //Serial.print("char myVar[]={'A','B','C','D','E','F','G','H','I','J','\0'}");
+    Serial.println("int myVar[]={1,2,3,4,9,3,7,2,5,1,3}");
+    //myPointer = &myVar[0];  // punta all'indirizzo del valore 0
+    myPointer = myVar;
+                            // *myPointer punta al contenuto
+    //Serial.print ("myPointer    : "); Serial.println(myPointer);
+    Serial.print ("-----------------");
+    type(myVar[]);
+    type(myPointer);
+    Serial.print ("-----------------");
 
-    //ptr2 = &test2;
-    //ptr3 = &test3;
+    Serial.print ("&myVar[0] uguale a *myPointer  : "); Serial.println(*myPointer);
 
+    myPointer = myPointer +1;
+    Serial.print
+    ("myPointer +1 : "); Serial.println(*myPointer);
 
+    myPointer = myPointer +2;
+    Serial.print ("myPointer +2 : "); Serial.println(*myPointer);
+
+    myPointer = sizeof(myVar);
+    Serial.print ("myPointer +2 : "); Serial.println(*myPointer);
 }
+
+
 
 void loop() {
-    //Serial.println("--__--");
-    //exit(0);
-
+    //return 0;
 }
 
+
+/*
+void setup() {
+  int *myPointer;
+  int myVar[5] = {1, 3, 5, 6, 8};
+  myPointer = &myVar[0]; // Holds the address of the 1st element.
+  myPointer = myPointer + 2;
+// Incremented by 2 to get 3rd value of our array.
+  Serial.begin(9600);
+  Serial.print(*myPointer);
+}
+*/
